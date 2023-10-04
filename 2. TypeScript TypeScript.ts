d8bc5656e48e3ab20a20a292f876537077124f51
Vsevolod Rukavishnikov <107400712+SevaDactyl@users.noreply.github.com>
2023-10-04 23:45:32 +0500

    TypeScript: TypeScript

---
+++ 2. TypeScript TypeScript.ts
@@ -0,0 +1,16 @@
+/*
+По образцу примера с суммой из урока напишите функцию, которая находит произведение переданных чисел:
+
+multiply(3, 8); // 24
+multiply(1, 2); // 2
+*/
+
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+
+// BEGIN (write your solution here)
+function multiply(a: number, b: number) {
+    return a * b;
+}
+// END
+
+export default multiply;
