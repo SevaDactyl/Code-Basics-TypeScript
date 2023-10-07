f4e1f39ef291743efe2bb951bfc48ef192c1ecb0
Vsevolod Rukavishnikov <107400712+SevaDactyl@users.noreply.github.com>
2023-10-07 23:22:48 +0500

    TypeScript: Anonymous Functions

---
+++ 5. TypeScript Anonymous Functions.ts
@@ -0,0 +1,11 @@
+/*
+Напишите функцию, которая возвращает массив четных чисел из массива numbers.
+*/
+
+const numbers = [1, 3, 8, 9, 100, 23, 55, 34];
+
+// BEGIN (write your solution here)
+const getEvenNumbers = () => numbers.filter((num) => num % 2 === 0);
+// END
+
+export default getEvenNumbers;
