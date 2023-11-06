9bf6b02f7532e28a7dc440a10865985a59e83cbe
Vsevolod Rukavishnikov <107400712+SevaDactyl@users.noreply.github.com>
2023-11-06 23:49:26 +0500

    TypeScript: Types as sets

---
+++ 25. TypeScript Types as sets.ts
@@ -0,0 +1,13 @@
+/*
+Определите тип CustomType, который может принимать следующие значения:
+
+null
+undefined
+числа
+*/
+
+// BEGIN (write your solution here)
+type CustomType = null | undefined | number;
+// END
+
+export default CustomType;
